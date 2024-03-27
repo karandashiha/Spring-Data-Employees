59849531131ae043b40d712a7a4d3dfe30af3ab4
Katy Reshetnyk <kreshetnyk@gmail.com>
2024-03-27 15:55:56 +0200

    Initial commit

---
+++ INFO/SQL.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS demo_db.employees
+( id INTEGER NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(128) NOT NULL,
+  last_name VARCHAR(128) NOT NULL,
+  position VARCHAR(128) NOT NULL,
+  phone VARCHAR(128) NOT NULL,
+  PRIMARY KEY (id)
+);
